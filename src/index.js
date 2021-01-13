38530c5f9226f62a8217637d4f4e342a1a8dfa5f
essencedrain <moonkero@naver.com>
2021-01-13 15:03:19 +0900

    1.4까지

--- src/index.js
+++ src/index.js
@@ -5,23 +5,84 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector("span");
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-const updateText = () => {
-  number.innerText = count;
-}
+const countModifier = (count = 0, action) => {
+
+  switch (action.type){
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count;
+  }
+
+  // console.log(count,action);
+  // if (action.type ==="ADD"){
+  //   return count + 1;
+  // } else if (action.type === "MINUS"){
+  //   return count - 1;
+  // } else{
+  //   return count;
+  // }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  // console.log(countStore.getState());
+  number.innerText = countStore.getState();
+
+};
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({type: "ADD"})
 }
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({type: "MINUS"})
 }
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", handleAdd );
+minus.addEventListener("click", handleMinus );
+
+
+// console.log(countStore.getState());
+
+
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"MINUS"});
+
+// console.log(countStore.getState());
+
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// }
+
+// const handleAdd = () => {
+//   count = count + 1;
+//   updateText();
+// }
+
+// const handleMinus = () => {
+//   count = count - 1;
+//   updateText();
+// }
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
